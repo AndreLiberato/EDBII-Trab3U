ff25cdb03c8f3472b7b624f789572110964db6d3
taleshrocha <taleshrochaz@gmail.com>
2022-07-16 17:43:31 -0300

    Bug in insert

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,9 @@ using namespace avlt;
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
 
-  AvlTree *avlTree = new AvlTree();
-  std::string command = "I4";
-  avlTree->from_command(avlTree->getRoot(), command);
+  AvlTree* avlTree = new AvlTree();
+  avlTree->insert(5, avlTree->getRoot(), true);
+  //std::cout << (((avlTree->getRoot())->right)->key) << std::endl;
   avlTree->print(avlTree->getRoot());
   delete avlTree;
 
